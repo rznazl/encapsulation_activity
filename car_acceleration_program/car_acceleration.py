15d411ef4e894f3d9877e99982b71b77ae7aaccd
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-06-01 21:41:29 +0800

    the accelerate part targets the private variable __speed and increment it by 5. meanwhile for the brake part, it decrases the speed by 5.it also doesn't allow negative values for acceleration; it will always return as 0

--- car_acceleration_program/car_acceleration.py
+++ car_acceleration_program/car_acceleration.py
@@ -2,4 +2,14 @@ class Car:
     def __init__(self, year_model, make):
         self.__year_model = year_model
         self.__make = make
-        self.__speed = 0 
+        self.__speed = 0
+
+    def accelerate(self):
+        self.__speed += 5
+
+    def brake(self):
+        self.__speed -= 5
+        if self.__speed < 0:
+            self.__speed = 0
+
+            
