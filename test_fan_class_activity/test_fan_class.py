25440089c4d9c7ca04174d2abaf0575e3fe192e3
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-06-01 19:24:09 +0800

    this is where the encapsulation begins. the double underscore essentially locks the variables from the outside wprld.

--- test_fan_class_activity/test_fan_class.py
+++ test_fan_class_activity/test_fan_class.py
@@ -3,3 +3,8 @@ class Fan:
     MEDIUM_MODE = 2
     FAST_MODE = 3
 
+    def __init__(self, speed=SLOW_MODE, radius =5.0, color="blue", on=False):
+        self.__speed = speed
+        self.__radius = float(radius)
+        self.__color = str(color)
+        self.__on = bool(on)
